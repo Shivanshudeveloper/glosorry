524f6f4d527216cb34d7d322b917c99d2c18ca1b
Shivanshu981 <developer.shivanshu@gmail.com>
2020-08-29 02:06:48 -0700

    Today Changes

--- App.js
+++ App.js
@@ -1,26 +1,57 @@
 // import { StatusBar } from "expo-status-bar";
-import React from "react";
+// import React, from "react";
+import React, { Component } from 'react';
 import { WebView } from "react-native-webview";
 // import { View } from "react-native";
+import { BackHandler } from 'react-native';
+
 import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
 import { SafeAreaView, StatusBar } from "react-native";
 
 function LoadingIndicatorView() {
   return <ActivityIndicator color="#009b88" size="large" />;
 }
-export default function App() {
+ export default class ExampleWebView extends Component {
+    webView = {
+      canGoBack: false,
+      ref: null,
+    }
+  
+    onAndroidBackPress = () => {
+      if (this.webView.canGoBack && this.webView.ref) {
+        this.webView.ref.goBack();
+        return true;
+      }
+      return false;
+    }
+  
+    componentWillMount() {
+      if (Platform.OS === 'android') {
+        BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
+      }
+    }
+  
+    componentWillUnmount() {
+      if (Platform.OS === 'android') {
+        BackHandler.removeEventListener('hardwareBackPress');
+      }
+    }
+    render() {
+
   return (
     <>
-      <StatusBar barStyle="dark-content"/>
+      <StatusBar barStyle="content" style={styles.top}/>
       <SafeAreaView style={styles.flexContainer}>
         <WebView
           source={{ uri: "https://fnfgrocery.com" }}
           startInLoadingState={true}
+          ref={(webView) => { this.webView.ref = webView; }}
+        onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
           renderLoading={() => (
             <ActivityIndicator
               color="black"
               size="large"
-              style={styles.flexContainer}
+              style={styles.loader}
             />
           )}
         />
@@ -28,12 +59,16 @@ export default function App() {
     </>
   );
 }
+  }
 const styles = StyleSheet.create({
   flexContainer: {
     flex: 1,
   },
+  loader: {
+    flex: 1,
+  },
   top: {
-    marginTop:"12px",
-    backgroundColor: "black",
+    marginTop:"1px",
+    backgroundColor: "white",
   }
 });
